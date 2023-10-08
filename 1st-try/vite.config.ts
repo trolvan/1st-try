0864bc481839d00554c7c66049852d4a3deb8ea5
杜嘉辉 <dujh@eastcom-sw.com>
2023-10-08 15:25:16 +0800

    feat: 引入element-plus

--- 1st-try/vite.config.ts
+++ 1st-try/vite.config.ts
@@ -4,11 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		vueJsx(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()]
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()]
+		}),
+		ElementPlus({
+			useSource: true
+		})
 	],
 	resolve: {
 		alias: {
