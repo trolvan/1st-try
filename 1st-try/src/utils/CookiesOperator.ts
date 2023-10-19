f7c6288a7f9429d7ce731cd7cc6195420140d6f8
杜嘉辉 <dujh@eastcom-sw.com>
2023-10-19 10:53:50 +0800

    chore: 优化注释

--- 1st-try/src/utils/CookiesOperator.ts
+++ 1st-try/src/utils/CookiesOperator.ts
@@ -1,7 +1,11 @@
 class CookiesOperator {
-	/* 添加cookie 或 根据键名更新对应cookie
-   * 能传入四个参数：cname-键名 | cVal-键值 | dVal-存在时间 | dType-时间类型 默认为日
-   */
+	/**
+	 * @description 添加cookie 或 根据键名更新对应cookie
+	 * @param cname cookie键名
+	 * @param cVal cookie键值
+	 * @param dVal 存在时间
+	 * @param dType 时间类型 默认为日
+	 */
 	setCookie(cname:string, cVal:any, dVal:number, dType?:string) {
 		const d:Date = new Date()
 		switch (dType) {
@@ -29,7 +33,9 @@ class CookiesOperator {
 		const expires = 'expires=' + d.toLocaleTimeString()
 		document.cookie = `${cname}=${cVal}; ${expires}`
 	}
-	// 根据键名获取cookie对应值
+	/**
+	 * @description 根据键名获取cookie对应值
+	 */
 	getCookie(cname: string) {
 		const name:string = cname + '='
 		const cArray:string[] = document.cookie.split(';')
@@ -39,7 +45,9 @@ class CookiesOperator {
 		})
 		return cookie ? cookie.substring(name.length + 1, cookie.length) : ''
 	}
-	// 根据键名去除对应cookie
+	/**
+	 * @description 根据键名去除对应cookie
+	 */
 	removeCookie(cname:string) {
 		document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`
 	}
