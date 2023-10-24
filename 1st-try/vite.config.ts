e020c9d978045eb312f5c6764862dd9b5cef4711
杜嘉辉 <dujh@eastcom-sw.com>
2023-10-24 17:42:58 +0800

    fix: 尝试修复动态组件404的问题

--- 1st-try/vite.config.ts
+++ 1st-try/vite.config.ts
@@ -13,7 +13,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './',
+	base: process.env.NODE_ENV === 'production' ? './' : '/',
 	plugins: [
 		vue(),
 		vueJsx(),
