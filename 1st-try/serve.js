2e2be410b5a50e8c74a678001502893dbe11bcca
杜嘉辉 <dujh@eastcom-sw.com>
2023-10-25 14:14:14 +0800

    feat: 添加本地build测试

---
+++ 1st-try/serve.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('./dist'))
+app.listen(8888, () => {
+	console.log('Server running at http://127.0.0.1:8888/')
+})
