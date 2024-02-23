13e0560d1f7ab9c68130b26979ece0ca405c4e68
杜嘉辉 <dujh@eastcom-sw.com>
2024-02-23 09:33:57 +0800

    feat: 封装el-scrollbar并保持样式和浏览器滚动条一致

---
+++ 1st-try/src/components/Scrollbar/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const $props = defineProps({
+	always: {
+		type: Boolean,
+		default: true
+	}
+})
+
+const attrs = { ...useAttrs(), always: $props.always }
+</script>
+
+<template>
+<el-scrollbar v-bind="attrs">
+  <slot />
+</el-scrollbar>
+</template>
+
+<style scoped lang="scss">
+:deep(.el-scrollbar__thumb):active {
+  background-color: $scrollbarThumbActiveColor;
+  opacity: $scrollbarHoverOpacity;
+}
+</style>
